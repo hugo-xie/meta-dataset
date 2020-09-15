f65829622c84656d2b9ffe589f5673fc4597733b
hugo-xie <xieyu9332@gmail.com>
2020-09-15 20:31:17 +0800

    test meta dataset

--- create.py
+++ create.py
@@ -6,4 +6,4 @@ json_path = '/home/xieyu/project/meta-dataset/meta-dataset/Records/ilsvrc_2012/d
 
 with open(json_path,'r') as load_f: 
     load_dict = json.load(load_f)
-    print(load_dict['images_per_class']['TRAIN'].keys())
+    print(len(load_dict['images_per_class']['TRAIN'].keys()))
