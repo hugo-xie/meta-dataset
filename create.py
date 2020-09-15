cc4fb9aa9281f4dec44e873f8e44622abe04dffe
hugo-xie <xieyu9332@gmail.com>
2020-09-15 21:31:43 +0800

    test meta dataset

--- create.py
+++ create.py
@@ -10,7 +10,13 @@ with open(json_path,'r') as load_f:
     count = 0
     imagenet_train = list(load_dict['images_per_class']['TEST'].keys())
     for subdir in imagenet_train:
-        if os.path.exists(os.path.join(ori_datadir, subdir)):
+        tem_dir_ori = os.path.join(ori_datadir, subdir)
+        if os.path.exists(tem_dir_ori):
+            tem_dir_target = os.path.join(tar_datadir, subdir)
+            os.mkdir(tem_dir_target)
+            cmd = 'ln -s {} {}'.format(tem_dir_ori, tem_dir_target)
+            os.system(cmd)
             count += 1
+
     
     print(count)
