0e73323cf9c3406f8cfcde5808618c587357df90
hugo-xie <xieyu9332@gmail.com>
2020-09-15 17:44:45 +0800

    test meta dataset

--- test.py
+++ test.py
@@ -15,6 +15,7 @@ from meta_dataset.data import pipeline
 import torch 
 from PIL import Image 
 import torchvision.transforms as transforms
+import moco.loader
 
 BASE_PATH = '/home/xieyu/project/meta-dataset/meta-dataset/Records'
 GIN_FILE_PATH = 'meta_dataset/learn/gin/setups/data_config.gin'
@@ -24,28 +25,6 @@ gin.parse_config_file(GIN_FILE_PATH)
 # Comment out to disable eager execution.
 tf.compat.v1.enable_eager_execution()
 # 4
-def iterate_dataset(dataset, n):
-  if not tf.executing_eagerly():
-    iterator = dataset.make_one_shot_iterator()
-    next_element = iterator.get_next()
-    with tf.Session() as sess:
-      for idx in range(n):
-        yield idx, sess.run(next_element)
-  else:
-    for idx, episode in enumerate(dataset):
-      if idx == n:
-        break
-      yield idx, episode
-
-SPLIT = learning_spec.Split.TRAIN
-
-BATCH_SIZE = 16
-ADD_DATASET_OFFSET = True
-dataset_records_path = os.path.join(BASE_PATH, 'ilsvrc_2012')
-dataset_spec = dataset_spec_lib.load_dataset_spec(dataset_records_path)
-dataset_batch = pipeline.make_one_source_batch_pipeline(
-    dataset_spec=dataset_spec, batch_size=BATCH_SIZE, split=SPLIT,
-    image_size=224)
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 augmentation = [
@@ -60,8 +39,67 @@ augmentation = [
             normalize
         ]
 trans = transforms.Compose(augmentation)
-for idx, ((images, labels), source_id) in iterate_dataset(dataset_batch, 1):
-    import pdb; pdb.set_trace()
-    images = images.numpy()
-    images = trans(images)
-    print(images.shape, labels.shape)
+
+to_torch_labels = lambda a: torch.from_numpy(a.numpy()).long()
+to_torch_imgs = lambda a: Image.fromarray(np.transpose(a.numpy(), (0, 3, 1, 2))[0])
+def iterate_dataset(dataset, n):
+  if not tf.executing_eagerly():
+    iterator = dataset.make_one_shot_iterator()
+    next_element = iterator.get_next()
+    with tf.Session() as sess:
+      for idx in range(n):
+        yield idx, sess.run(next_element)
+  else:
+    for idx, episode in enumerate(dataset):
+      if idx == n:
+        break
+      yield idx, episode
+
+def iterate_dataset_batch(dataset, num_batches, batch_size):
+    if not tf.executing_eagerly():
+        iterator = dataset.make_one_shot_iterator()
+        next_element = iterator.get_next()
+        with tf.Session() as sess:
+            for idx in range(num_batches):
+                episode, source_id = sess.run(next_element)
+                
+                yield(to_torch_imgs(episode[0]), to_torch_labels(episode[1]))
+    
+    else: 
+        batch_count = 0
+        curr_batch = []
+        for idx, (episode, source_id) in enumerate(dataset):
+            if batch_count == num_batches:
+                break 
+            images = moco.loader.TwoCropsTransform(to_torch_imgs(episode[0]))
+            batch_entry = [images[0], images[1], to_torch_labels(episode[1])]
+            curr_batch.append(batch_entry)
+            if len(curr_batch) == batch_size:
+                images_q = torch.stack([k[0] for k in curr_batch])
+                images_k = torch.stack([k[1] for k in curr_batch])
+                labels = torch.stack([k[2] for k in curr_batch])
+                curr_batch = []
+                batch_count += 1
+                yield images_q, images_k, labels
+
+
+
+
+
+SPLIT = learning_spec.Split.TRAIN
+
+BATCH_SIZE = 1
+ADD_DATASET_OFFSET = True
+dataset_records_path = os.path.join(BASE_PATH, 'ilsvrc_2012')
+dataset_spec = dataset_spec_lib.load_dataset_spec(dataset_records_path)
+dataset_batch = pipeline.make_one_source_batch_pipeline(
+    dataset_spec=dataset_spec, batch_size=BATCH_SIZE, split=SPLIT,
+    image_size=224)
+
+for idx, (images_q, images_k, labels) in iterate_dataset_batch(dataset_batch, 1, 256):
+    # import pdb; pdb.set_trace()
+    # images = images.numpy()
+    # images = trans(images)
+    # print(images.shape, labels.shape)
+    print(images_q.shape)
+    print(images_k.shape)
