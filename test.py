b8999d5dad14c7d7b7f8a4e9ec5534693aaa5fd2
hugo-xie <xieyu9332@gmail.com>
2020-09-15 18:15:58 +0800

    test meta dataset

--- test.py
+++ test.py
@@ -81,7 +81,7 @@ def iterate_dataset_batch(dataset, num_batches, batch_size):
             k_batch.append(images[0])
             q_batch.append(images[1])
             label_batch.append(to_torch_labels(episode[1]))
-            if len(curr_batch) == batch_size:
+            if len(k_batch) == batch_size:
                 images_q = torch.stack(q_batch)
                 images_k = torch.stack(k_batch)
                 labels = torch.stack(label_batch)
