59781c91b7e833abeae2c14a92c3da9b8a41e176
hugo-xie <xieyu9332@gmail.com>
2020-09-15 17:58:57 +0800

    test meta dataset

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ augmentation = [
 trans = transforms.Compose(augmentation)
 
 to_torch_labels = lambda a: torch.from_numpy(a.numpy()).long()
-to_torch_imgs = lambda a: np.transpose(a.numpy(), (0, 3, 1, 2))
+to_torch_imgs = lambda a: torch.from_numpy(np.transpose(a.numpy(), (0, 3, 1, 2)))
 def iterate_dataset(dataset, n):
   if not tf.executing_eagerly():
     iterator = dataset.make_one_shot_iterator()
@@ -71,7 +71,7 @@ def iterate_dataset_batch(dataset, num_batches, batch_size):
         for idx, (episode, source_id) in enumerate(dataset):
             if batch_count == num_batches:
                 break 
-            images = Image.fromarray((to_torch_imgs(episode[0])[0]*255).astype(np.uint8))
+            images = to_torch_imgs(episode[0])
             images = moco.loader.TwoCropsTransform(images)
             batch_entry = [images[0], images[1], to_torch_labels(episode[1])]
             curr_batch.append(batch_entry)
